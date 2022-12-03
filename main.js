ce274070272c2857033c066167802fff05c1548e
Litauszki György <litauszki16@gmail.com>
2022-12-03 23:26:48 +0100

    Made progress on sidenav, started first rulebook page

--- main.js
+++ main.js
@@ -6,17 +6,19 @@ $(document).ready(function (){
 
     $("#showbtn").click(function (){
         $(".sidenav")[0].style.width = "400px"
+        $(".dimmer")[0].style.display = "block"
     })
 
     $("#closebtn").click(function (){
         $(".dropdown-content")[0].style.height = "0"
         $(".dropdown-content2")[0].style.height = "0"
         $(".sidenav")[0].style.width = "0px"
+        $(".dimmer")[0].style.display = "none"
     })
 
     $(".dropbtn").click(function (){
         if (!listOpen){
-            $(".dropdown-content")[0].style.height = "23%"
+            $(".dropdown-content")[0].style.height = "45%"
             listOpen = true
         }else{
             $(".dropdown-content")[0].style.height = "0"
@@ -28,13 +30,23 @@ $(document).ready(function (){
 
     $(".dropbtn2").click(function (){
         if (!list2Open){
-            $(".dropdown-content")[0].style.height = "35%"
-            $(".dropdown-content2")[0].style.height = "31%"
+            $(".dropdown-content")[0].style.height = "44%"
+            $(".dropdown-content2")[0].style.height = "100%"
             list2Open = true
         }else{
-            $(".dropdown-content")[0].style.height = "23%"
+            $(".dropdown-content")[0].style.height = "45%"
             $(".dropdown-content2")[0].style.height = "0%"
             list2Open = false
         }
+    })
+
+    $(".wholeContent").click(function (){
+        let content = $("#content")
+        content.attr("src", "../../rulebooks/7%20Wonders.pdf")
+    })
+
+    $(".reference").click(function (){
+        let content = $("#content")
+        content.attr("src", "../../rulebooks/7%20Wonders%20Rules%20Reference.pdf")
     })
 })
